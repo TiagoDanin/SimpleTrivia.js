aefe5e96d105f5d748e0409127ab837b342d6a4d
TiagoDanin <TiagoDanin@outlook.com>
2020-02-09 10:02:51 -0300

    feat: random questions

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # SimpleTrivia.js
+
+[![Travis](https://img.shields.io/travis/TiagoDanin/SimpleTrivia.js.svg?branch=master&style=flat-square)](https://travis-ci.org/TiagoDanin/SimpleTrivia.js) [![Downloads](https://img.shields.io/npm/dt/simpletriviajs.svg?style=flat-square)](https://npmjs.org/package/simpletriviajs) [![Version](https://img.shields.io/npm/v/simpletriviajs.svg?style=flat-square)](https://npmjs.org/package/simpletriviajs) [![XO code style](https://img.shields.io/badge/code%20style-XO-red.svg?style=flat-square)](https://github.com/xojs/xo) 
+
 Trival Game Web using Open Trivia DB API
+
+## Features
+
+- Version with jQuery
+- Fun Game :)
+
+## Installation
+
+Module available through the [npm registry](https://www.npmjs.com/). It can be installed using the [`npm`](https://docs.npmjs.com/getting-started/installing-npm-packages-locally) or [`yarn`](https://yarnpkg.com/en/) command line tool.
+
+```sh
+# Yarn (Recomend)
+yarn add simpletriviajs
+# NPM 
+npm install simpletriviajs --save
+```
+
+## Usage
+
+- [tiagodanin.github.io/SimpleTrivia.js/](https://tiagodanin.github.io/SimpleTrivia.js/)
+
+## Tests
+
+To run the test suite, first install the dependencies, then run `test`:
+
+```sh
+# Using Yarn
+yarn test
+```
+
+## Dependencies
+
+None
+
+## Dev Dependencies
+
+<details>
+	<summary><a href="https://ghub.io/xo">xo</a>: JavaScript linter with great defaults</summary>
+	<b>Author</b>: Sindre Sorhus</br>
+	<b>License</b>: MIT</br>
+	<b>Version</b>: ^0.24.0
+</details>
+
+## Contributors
+
+Pull requests and stars are always welcome. For bugs and feature requests, please [create an issue](https://github.com/TiagoDanin/SimpleTrivia.js/issues). [List of all contributors](https://github.com/TiagoDanin/SimpleTrivia.js/graphs/contributors).
+
+## License
+
+[MIT](LICENSE) © [Tiago Danin](https://TiagoDanin.github.io) & Eduardo Mouta
--- index.js
+++ index.js
@@ -1,64 +1,96 @@
+/* eslint-disable no-undef */
+
 let score = 0
 let trival = {}
+
+const colors = {
+	green: '#32d296',
+	red: '#f0506e'
+}
+
 const apiBase = 'https://opentdb.com/api.php?amount=1&type=multiple'
 
+const useXHR = url => new Promise((resolve, reject) => {
+	try {
+		const xhr = new XMLHttpRequest()
+		xhr.withCredentials = false
+
+		xhr.open('GET', url)
+		xhr.addEventListener('load', () => resolve(xhr.responseText))
+		xhr.addEventListener('error', () => reject(xhr.statusText))
+		xhr.send()
+	} catch (error) {
+		reject(error)
+	}
+})
+
 const showQuestion = () => {
-    document.getElementById('app').hidden = false
-    const questions = [...trival.incorrect_answers, trival.correct_answer] // TODO random 
+	document.querySelector('#app').hidden = false
+	const questions = [...trival.incorrect_answers, trival.correct_answer].sort(() => 0.5 - Math.random())
 
-    let htmlAnswers = '<h2 class="uk-card-title">Answers</h2>\n'
-    for (let question of questions) {
-        question = question.charAt(0).toUpperCase() + question.slice(1);
-        htmlAnswers += `<button style="margin-bottom: 3%;" type="button" class="bt-select uk-button uk-text-large">${question}</button>\n`
-    }
+	let htmlAnswers = '<h2 class="uk-card-title">Answers</h2>\n'
+	for (let question of questions) {
+		question = question.charAt(0).toUpperCase() + question.slice(1)
+		htmlAnswers += `<button style="margin-bottom: 3%;" type="button" class="bt-select uk-button uk-text-large">${question}</button>\n`
+	}
 
-    document.getElementById('answers').innerHTML = htmlAnswers
-    document.getElementById('question').innerHTML = trival.question
-    document.getElementById('title').innerText = `Category: ${trival.category} (${trival.difficulty})`
-    for (const answer of document.getElementsByClassName('bt-select')) {
-        answer.addEventListener('click', checkAnswer, false)
-    }
+	document.querySelector('#answers').innerHTML = htmlAnswers
+	document.querySelector('#question').innerHTML = trival.question
+	document.querySelector('#title').innerText = `Category: ${trival.category} (${trival.difficulty})`
+	for (const answer of document.querySelectorAll('.bt-select')) {
+		answer.addEventListener('click', checkAnswer, false)
+	}
 }
 
 const getQuestions = () => {
-    return fetch(apiBase)
-        .then(res => res.json())
-        .then(data => {
-            if (data.response_code == 0) {
-                trival = data.results[0]
-                showQuestion()
-            }
-        }).catch(error => console.error)
+	if (typeof window.fetch === 'undefined') {
+		window.fetch = useXHR
+	}
+
+	return fetch(apiBase)
+		.then(res => typeof res.json === 'undefined' ? JSON.parse(res) : res.json())
+		.then(data => {
+			if (data.response_code === 0) {
+				trival = data.results[0]
+				showQuestion()
+			}
+		}).catch(error => console.error(error))
 }
 
-const checkAnswer = async(eventClick) => {
-    for (const answer of document.getElementsByClassName('bt-select')) {
-        answer.removeEventListener('click', checkAnswer)
-    }
+const checkAnswer = async eventClick => {
+	for (const answer of document.querySelectorAll('.bt-select')) {
+		answer.removeEventListener('click', checkAnswer)
+		if (trival.correct_answer.toUpperCase() === answer.innerText.toUpperCase()) {
+			answer.style.borderColor = colors.green
+			answer.style.borderStyle = 'solid'
+		}
+	}
 
-    if (trival.correct_answer.toUpperCase() === eventClick.target.innerText.toUpperCase()) {
-        switch (trival.difficulty) {
-            case 'easy':
-                score += 5;
-                break;
-            case 'medium':
-                score += 10;
-                break;
-            case 'hard':
-                score += 20;
-                break;
-            default:
-                score += 5;
-        }
-        document.getElementById('score-point').innerText = `Points: ${score}`
-        eventClick.target.style.background = '#32d296'
-    } else {
-        eventClick.target.style.background = '#f0506e'
-    }
-    eventClick.target.style.color = 'black'
+	if (trival.correct_answer.toUpperCase() === eventClick.target.innerText.toUpperCase()) {
+		switch (trival.difficulty) {
+			case 'easy':
+				score += 5
+				break
+			case 'medium':
+				score += 10
+				break
+			case 'hard':
+				score += 20
+				break
+			default:
+				score += 5
+		}
 
-    await new Promise(resolve => setTimeout(resolve, 1500));
-    getQuestions()
+		document.querySelector('#score-point').innerText = `Points: ${score}`
+		eventClick.target.style.background = colors.green
+	} else {
+		eventClick.target.style.background = colors.red
+	}
+
+	eventClick.target.style.color = 'black'
+
+	await new Promise(resolve => setTimeout(resolve, 1500))
+	getQuestions()
 }
 
 getQuestions()
